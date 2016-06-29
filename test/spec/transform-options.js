97d0d2315991f06d4489dac276a410f809232813
muji <freeformsystems@gmail.com>
2016-06-29 19:49:30 +0800

    Complete transform phase test specs.

---
+++ test/spec/transform-options.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect
+  , trucks = require('../../lib');
+
+describe('transform:', function() {
+
+  it('should handle no options', function(done) {
+    trucks.transform(
+      {js: []}, (err, result) => {
+        expect(err).to.eql(null);
+        expect(result).to.be.an('object');
+        done();
+    });
+  });
+
+  it('should transform with plugins option', function(done) {
+    trucks.transform(
+      {js: [{contents: 'skate.define("x-foo", {});var foo = bar();'}]},
+      {babel: {plugins: []}},
+      (err, result) => {
+        expect(err).to.eql(null);
+        expect(result).to.be.an('object');
+        done();
+    });
+  });
+
+});

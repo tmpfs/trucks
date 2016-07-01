33c75b23334bac7a3c49109bb10021a9d5e17f1f
muji <freeformsystems@gmail.com>
2016-07-01 19:58:22 +0800

    Add test spec for compiling multiple templates.

--- test/spec/compiler/map-multiple.js
+++ test/spec/compiler/map-multiple.js
@@ -4,13 +4,18 @@ var expect = require('chai').expect
 
 describe('compiler:', function() {
 
-  it('should generate AST map for template element', function(done) {
-    const tpl = '<template id="x-foo"><span></span></template>';
+  it('should generate AST map for multiple template elements', function(done) {
+    const tpl = '<template id="x-foo"><span></span></template>'
+      + '<template id="x-bar"><span></span></template>';
+
     const res = trucks.compile(tpl);
     const result = babel.transformFromAst(res.map);
     expect(result.code).to.eql(
       'const templates = {\n'
         + '  "x-foo": function render(elem) {\n'
+        + '    skate.vdom.element("span");\n'
+        + '  },\n'
+        + '  "x-bar": function render(elem) {\n'
         + '    skate.vdom.element("span");\n'
         + '  }\n'
         + '};'

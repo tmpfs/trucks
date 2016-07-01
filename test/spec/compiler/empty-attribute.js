1ca49140f5758f702179ce421abda862ceb9988e
muji <freeformsystems@gmail.com>
2016-07-01 12:49:16 +0800

    Add test spec for empty attribute.

---
+++ test/spec/compiler/empty-attribute.js
@@ -0,0 +1,29 @@
+var expect = require('chai').expect
+  , babel = require('babel-core')
+  , trucks = require('../../../lib');
+
+describe('compiler:', function() {
+
+  it('should generate AST for element with empty attribute', function(done) {
+    const tpl = '<template id="x-foo">'
+      + '<span icon></span></template>';
+
+    const res = trucks.compile(tpl);
+
+    expect(res).to.be.an('array').to.have.length(1);
+
+    // component id
+    expect(res[0].id).to.eql('x-foo');
+
+    // function body AST
+    expect(res[0].body).to.be.an('object');
+
+    const result = babel.transformFromAst(res[0].body);
+    expect(result.code).to.eql('skate.vdom.element("span", {\n'
+      + '  icon: ""\n'
+      + '});');
+
+    done();
+  });
+
+});

a8e6502a86174539d4b431c6ffcc3f3d1c0d46ba
muji <freeformsystems@gmail.com>
2016-07-03 14:38:04 +0800

    Update test spec.

--- test/spec/unknown-callee.js
+++ test/spec/unknown-callee.js
@@ -15,10 +15,12 @@ describe('trucks:', function() {
       (err, result) => {
         expect(err).to.eql(null);
         expect(result).to.be.an('object');
-        expect(result.options).to.be.an('object');
 
         // NOTE: not too much to assert here, this spec
         // NOTE: triggers some code paths
+        // NOTE: but check that the unknown callee expression (function call)
+        // NOTE: is preserved in the result javascript
+        expect(/foo\(\);/m.test(result.javascript)).to.eql(true);
 
         done();
       }

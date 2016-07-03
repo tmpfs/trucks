1a5ab8bae9fc6090d98acbc6b53ab3014bb1bd2f
muji <freeformsystems@gmail.com>
2016-07-03 13:13:29 +0800

    Add test spec for trim options.

---
+++ test/spec/trim-options.js
@@ -0,0 +1,22 @@
+var expect = require('chai').expect
+  , trucks = require('../../lib');
+
+describe('trucks:', function() {
+
+  it('should compile with trim options disabled', function(done) {
+    const src = 'test/fixtures/simple-inline/components.html';
+    trucks(
+      {
+        files: [src],
+        out: 'target',
+        name: 'trim-options',
+        trim: {newlines: false, lines: false}
+      }, (err, result) => {
+        expect(err).to.eql(null);
+        expect(result).to.be.an('object');
+        done();
+      }
+    );
+  });
+
+});

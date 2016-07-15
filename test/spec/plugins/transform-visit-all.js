8790aefa9f4fd2d426a937d5f35e9ace227be7bc
muji <freeformsystems@gmail.com>
2016-07-15 12:43:22 +0800

    Improve transform visitor test specs.

--- test/spec/plugins/transform-visit-all.js
+++ test/spec/plugins/transform-visit-all.js
@@ -3,11 +3,12 @@ var expect = require('chai').expect
 
 describe('transform:', function() {
 
-  it('should visit template nodes', function(done) {
+  it('should visit all nodes', function(done) {
     const src = 'test/fixtures/simple-inline/components.html';
   
     let visited = false
-      , count = 0;
+      , count = 0
+      , nodes = [];
 
     trucks(
       {
@@ -18,8 +19,9 @@ describe('transform:', function() {
         conf: {
           transform: {
             visitors: [{
-              'Template': function(node, cb) {
+              '*': function(node, cb) {
                 expect(node).to.be.an('object');
+                nodes.push(node);
                 visited = true;
                 count++;
                 cb(null, node);
@@ -33,7 +35,21 @@ describe('transform:', function() {
         expect(state).to.be.an('object');
 
         expect(visited).to.eql(true);
-        expect(count).to.eql(1);
+        expect(count).to.eql(6);
+
+        const components = state.components
+            , File = components.File
+            , Module = components.Module
+            , Template = components.Template
+            , Style = components.Style
+            , Script = components.Script;
+
+        expect(nodes[0]).to.be.instanceof(File);
+        expect(nodes[1]).to.be.instanceof(File);
+        expect(nodes[2]).to.be.instanceof(Module);
+        expect(nodes[3]).to.be.instanceof(Template);
+        expect(nodes[4]).to.be.instanceof(Style);
+        expect(nodes[5]).to.be.instanceof(Script);
 
         expect(state.options).to.be.an('object');
         expect(state.tree).to.be.an('object');

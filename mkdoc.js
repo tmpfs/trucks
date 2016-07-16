40d1c29a78203981acb006399d96416c9ece1651
muji <freeformsystems@gmail.com>
2016-07-16 10:19:46 +0800

    Tidy mkdoc source file.

--- mkdoc.js
+++ mkdoc.js
@@ -1,5 +1,19 @@
 var mk = require('mktask');
 
+function doc(src, dest, opts, cb) {
+  mk.doc(src)
+    .pipe(mk.pi())
+    .pipe(mk.ref())
+    .pipe(mk.abs())
+    .pipe(mk.msg())
+    .pipe(mk.toc(opts.toc))
+    //.pipe(mk.ast.stringify())
+    //.pipe(process.stdout)
+    .pipe(mk.out())
+    .pipe(mk.dest(dest))
+    .on('finish', cb);
+}
+
 // @task api build the api docs.
 function api(cb) {
   // build intermediary file
@@ -8,83 +22,43 @@ function api(cb) {
     'mkapi src/index.js --level=3 '
       + '> doc/api/api-docs.md');
 
-  // build api docs with toc
-  mk.doc('doc/api/api.md')
-    .pipe(mk.pi())
-    .pipe(mk.ref())
-    .pipe(mk.abs())
-    .pipe(mk.msg())
-    .pipe(mk.toc({depth: 2}))
-    .pipe(mk.out())
-    .pipe(mk.dest('doc/API.md'))
-    .on('finish', cb);
+  // build the docs
+  doc('doc/api/api.md', 'doc/API.md', {toc: {depth: 2}}, cb);
 }
 
 // @task options build the options file
 function options(cb) {
-  mk.doc('doc/options/options.md')
-    .pipe(mk.pi())
-    .pipe(mk.ref())
-    .pipe(mk.abs())
-    .pipe(mk.msg())
-    .pipe(mk.toc({depth: 2, max: 3}))
-    .pipe(mk.out())
-    .pipe(mk.dest('doc/OPTIONS.md'))
-    .on('finish', cb);
+  doc(
+    'doc/options/options.md', 'doc/OPTIONS.md',
+    {toc: {depth: 2, max: 3}}, cb);
 }
 
 // @task developer build the developer file
 function developer(cb) {
-  mk.doc('doc/developer/developer.md')
-    .pipe(mk.pi())
-    .pipe(mk.ref())
-    .pipe(mk.abs())
-    .pipe(mk.msg())
-    .pipe(mk.toc({depth: 2, max: 3}))
-    .pipe(mk.out())
-    .pipe(mk.dest('doc/DEVELOPER.md'))
-    .on('finish', cb);
+  doc(
+    'doc/developer/developer.md', 'doc/DEVELOPER.md',
+    {toc: {depth: 2, max: 3}}, cb);
 }
 
 // @task compiler build the compiler file
 function compiler(cb) {
-  mk.doc('doc/compiler/compiler.md')
-    .pipe(mk.pi())
-    .pipe(mk.ref())
-    .pipe(mk.abs())
-    .pipe(mk.msg())
-    .pipe(mk.toc({depth: 2, max: 3}))
-    .pipe(mk.out())
-    .pipe(mk.dest('doc/COMPILER.md'))
-    .on('finish', cb);
+  doc(
+    'doc/compiler/compiler.md', 'doc/COMPILER.md',
+    {toc: {depth: 2, max: 3}}, cb);
 }
 
 // @task exmaple build the example file
 function example(cb) {
-  mk.doc('doc/example/example.md')
-    .pipe(mk.pi())
-    .pipe(mk.ref())
-    .pipe(mk.abs())
-    .pipe(mk.msg())
-    .pipe(mk.toc({depth: 2, max: 3}))
-    .pipe(mk.out())
-    .pipe(mk.dest('doc/EXAMPLE.md'))
-    .on('finish', cb);
+  doc(
+    'doc/example/example.md', 'doc/EXAMPLE.md',
+    {toc: {depth: 2, max: 3}}, cb);
 }
 
 // @task readme build the readme file
 function readme(cb) {
-  mk.doc('doc/readme.md')
-    .pipe(mk.pi())
-    .pipe(mk.ref())
-    .pipe(mk.abs())
-    .pipe(mk.msg())
-    .pipe(mk.toc({depth: 2, max: 3}))
-    //.pipe(mk.ast.stringify())
-    //.pipe(process.stdout)
-    .pipe(mk.out())
-    .pipe(mk.dest('README.md'))
-    .on('finish', cb);
+  doc(
+    'doc/readme.md', 'README.md',
+    {toc: {depth: 2, max: 3}}, cb);
 }
 
 // @task docs build all docs

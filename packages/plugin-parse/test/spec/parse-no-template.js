8df6ac0df8ae0ec6ea0ba350a50622e366af2fb2
muji <freeformsystems@gmail.com>
2016-07-17 16:40:14 +0800

    Complete migration of parse test specs.

---
+++ packages/plugin-parse/test/spec/parse-no-template.js
@@ -0,0 +1,22 @@
+var expect = require('chai').expect
+  , trucks = require('../../../../src');
+
+describe('parse:', function() {
+
+  it('should parse without template element', function(done) {
+    const src = '../../test/fixtures/no-template/components.html';
+    trucks(
+      {
+        files: [src],
+        out: 'target',
+        name: 'parse-no-template',
+        plugins: [trucks.LOAD, require('../../src')]
+      }, (err, result) => {
+        expect(err).to.eql(null);
+        expect(result).to.be.an('object');
+        done();
+      }
+    );
+  });
+
+});

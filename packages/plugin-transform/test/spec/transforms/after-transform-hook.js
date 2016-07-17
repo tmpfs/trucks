fec79cf75929f15c2fd3993ea1322fc6f10d9149
muji <freeformsystems@gmail.com>
2016-07-17 16:51:19 +0800

    Assert on transform before/after hooks.

---
+++ packages/plugin-transform/test/spec/transforms/after-transform-hook.js
@@ -0,0 +1,56 @@
+var expect = require('chai').expect
+  , trucks = require('../../../../../src');
+
+describe('transform:', function() {
+
+  it('should use after transform function', function(done) {
+    const src = '../../test/fixtures/simple-inline/components.html'
+        , config = {
+            foo: 'bar'
+          };
+
+    let called = false
+      , invoked = false;
+
+    trucks(
+      {
+        files: [src],
+        plugins: [trucks.LOAD, trucks.PARSE, require('../../../src')],
+        out: 'target',
+        name: 'after-plugin-function',
+        after: {
+          transforms: [
+            function mock(state, conf) {
+              expect(conf).to.eql(config);
+              called = true;
+              return {
+                'Module': function(node, cb) {
+                  expect(node).to.be.an('object')
+                  invoked = true;
+                  cb();
+                }
+              };
+            }
+          ]
+        },
+        conf: {
+          transforms: {
+            mock: config
+          }
+        }
+      },
+      (err, state) => {
+        console.dir(err);
+        expect(err).to.eql(null);
+        expect(state).to.be.an('object');
+        expect(state.options).to.be.an('object');
+        expect(state.tree).to.be.an('object');
+
+        expect(called).to.eql(true);
+        expect(invoked).to.eql(true);
+        done();
+      }
+    );
+  });
+
+});
---
+++ packages/plugin-transform/test/spec/transforms/before-transform-hook.js
@@ -0,0 +1,55 @@
+var expect = require('chai').expect
+  , trucks = require('../../../../../src');
+
+describe('transform:', function() {
+
+  it('should use before transform function', function(done) {
+    const src = '../../test/fixtures/simple-inline/components.html'
+        , config = {
+            foo: 'bar'
+          };
+
+    let called = false
+      , invoked = false;
+
+    trucks(
+      {
+        files: [src],
+        plugins: [trucks.LOAD, trucks.PARSE, require('../../../src')],
+        out: 'target',
+        name: 'before-plugin-function',
+        before: {
+          transforms: [
+            function mock(state, conf) {
+              expect(conf).to.eql(config);
+              called = true;
+              return {
+                'Module': function(node, cb) {
+                  expect(node).to.be.an('object')
+                  invoked = true;
+                  cb();
+                }
+              };
+            }
+          ]
+        },
+        conf: {
+          transforms: {
+            mock: config
+          }
+        }
+      },
+      (err, state) => {
+        expect(err).to.eql(null);
+        expect(state).to.be.an('object');
+        expect(state.options).to.be.an('object');
+        expect(state.tree).to.be.an('object');
+
+        expect(called).to.eql(true);
+        expect(invoked).to.eql(true);
+        done();
+      }
+    );
+  });
+
+});

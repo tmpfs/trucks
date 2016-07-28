75a96bd0b421a9c2c7511e5819677774bf08b7fb
muji <freeformsystems@gmail.com>
2016-07-28 09:08:58 +0800

    Update bundle transform test specs.

---
+++ packages/transform-bundle/test/spec/bundle-empty-lists.js
@@ -0,0 +1,30 @@
+var expect = require('chai').expect
+  , trucks = require('../../../../src');
+
+describe('bundle:', function() {
+
+  it('should handle empty lists', function(done) {
+    const src = '../../test/fixtures/simple-inline/components.html';
+    trucks(
+      {
+        files: [src],
+        out: 'target',
+        name: 'bundle-empty-lists',
+        transforms: [require('../../src')],
+        bundle: {
+          before: {
+            css: null
+          },
+          after: {
+            css: []
+          }
+        }
+      }, (err, state) => {
+        expect(err).to.eql(null);
+        expect(state).to.be.an('object');
+        done();
+      }
+    );
+  });
+
+});
---
+++ packages/transform-bundle/test/spec/bundle-enoent.js
@@ -0,0 +1,30 @@
+var expect = require('chai').expect
+  , trucks = require('../../../../src');
+
+describe('bundle:', function() {
+
+  it('should handle enoent error', function(done) {
+    const src = '../../test/fixtures/simple-inline/components.html';
+    trucks(
+      {
+        files: [src],
+        out: 'target',
+        name: 'bundle-enoent',
+        transforms: [require('../../src')],
+        bundle: {
+          before: {
+            css: ['test/fixtures/non-existent.css']
+          }
+        }
+      },
+      (err) => {
+        function fn() {
+          throw err;
+        }
+        expect(fn).throws(/ENOENT/);
+        done();
+      }
+    );
+  });
+
+});
